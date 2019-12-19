3cffa957f2f1e54d35dd11f38cc07eabd94f228b
Ashley Ng <ashley.ng@quanturi.com>
2019-12-19 02:34:39 +0200

    stripe integration

---
+++ src/components/stripe-button/StripeButton.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_MC79yRmMaODTE96dTmPoUKph00XJrA6TpR';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Succesful!');
+    };
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='makes Scents'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is €${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
